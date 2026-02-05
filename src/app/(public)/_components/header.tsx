5e8ff7b9bba16677cddd6d78fa8f582b9bf480e4
Santiago Teixeira Arquete <santiagoarquete@gmail.com>
2026-02-04 22:18:56 -0300

    Commit

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -1,4 +1,6 @@
-import { headers } from "next/headers"
+"use client"
+
+import { useState } from "react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import {
@@ -11,9 +13,37 @@ import {
     SheetTitle,
     SheetTrigger,
 } from "@/components/ui/sheet"
-import { EllipsisVertical, Menu, MenuIcon } from "lucide-react"
+import { MenuIcon } from "lucide-react"
 
 export function Header() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
+    const navLinks = [
+        { href: "#profissionais", label: "Profissionais" },
+    ]
+
+    const NavLinks = () => (
+        <>
+            {navLinks.map((itens) => (
+                <Button
+                    key={itens.href}
+                    asChild
+                    onClick={() => setIsOpen(false)}
+                    className="bg-transparent hover:text-white text-black "
+                >
+                    <Link
+                        href={itens.href}
+                    >
+                        {itens.label}
+                    </Link>
+                </Button>
+            ))
+            }
+        </>
+    )
+
     return (
         <header className="fixed top-0 right-0 left-0 z-999 py-4 px-6 bg-white shadow-md">
             <div className="container mx-auto flex items-center justify-between">
@@ -21,28 +51,28 @@ export function Header() {
                     Odonto<span className="text-emerald-600 ">Plus</span>
                 </Link>
 
-                <nav>
-                    <a href="#" className="hidden md:flex items-center">Profissionais</a>
+                <nav className="hidden md:flex items-center">
+                    <NavLinks />
                 </nav>
 
-                <Sheet>
+                <Sheet open={isOpen} onOpenChange={setIsOpen}>
                     <SheetTrigger asChild className="md:hidden">
                         <Button
-                            className=""
                             size="icon"
                         >
-                            <MenuIcon className="h-6 w-6" />
+                            <MenuIcon className="w-6 h-6" />
                         </Button>
                     </SheetTrigger>
 
-                    <SheetContent side="right" className="w-240px sm:w-300px z-999">
-                        <SheetTitle>Menu</SheetTitle>
-                        <SheetHeader></SheetHeader>
+                    <SheetContent side="right" className="w-[250px] sm:w-[300px] z-[9999]">
+                        <SheetTitle className="px-5 pt-4 font-bold text-2xl">Menu</SheetTitle>
+                        <SheetDescription
+                            className="px-5">
+                            Navegue pelo site utilizando as opções abaixo:
+                        </SheetDescription>
 
-                        <SheetDescription>Veja nosso links</SheetDescription>
-
-                        <nav>
-                            <a href="#">Profissionais</a>
+                        <nav className="flex flex-col space-y-4 mt-6 shadow-md">
+                            <NavLinks />
                         </nav>
                     </SheetContent>
 
